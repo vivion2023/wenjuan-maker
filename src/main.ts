68f009ea5c985d5f8e6406aeb39d55c00b70c4f7
vivion <vivonhyt@gmail.com>
2025-03-04 09:53:30 +0800

    引入全局样式，去除body margin

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
+
 import App from "./App.vue";
 import router from "./router/index";
 import store from "@/store/index";
+
+import "@/assets/styles/global.scss";
 
 const app = createApp(App);
 app.use(router);
