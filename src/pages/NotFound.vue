db3179307b316d8581eb8487b8b96ce0cce0e18b
vivion <vivonhyt@gmail.com>
2025-03-05 20:38:26 +0800

    NotFound页面实现

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,11 +1,37 @@
 <template>
-  <div>
-    <h2>404 NotFound</h2>
+  <div class="container">
+    <Result
+      status="404"
+      title="404"
+      sub-title="Sorry, the page you visited does not exist."
+    >
+      <template #extra>
+        <Button type="primary" @click="() => router.push(HOME_PATHNAME)">
+          Back Home
+        </Button>
+      </template>
+    </Result>
   </div>
 </template>
 
 <script>
-export default {};
+import { Button, Result } from "ant-design-vue";
+import { useRouter } from "vue-router";
+import { HOME_PATHNAME } from "@/router";
+export default {
+  components: { Result, Button },
+  setup() {
+    const router = useRouter();
+    return { router, HOME_PATHNAME };
+  },
+};
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 64px - 66px);
+}
+</style>
