9d9718b1a22f3b62cf590b75381dbbbbf2bb2860
vivion <vivonhyt@gmail.com>
2025-03-17 19:43:07 +0800

    实现首页样式

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="container">
     <div class="info">
-      <Title>问卷调查</Title>
-      <Paragraph>已累计创建问卷100份，发布问卷90份，收到答卷900份</Paragraph>
-
-      <div>
-        <Button
-          type="primary"
-          @click="() => router.push(MANAGER_INDEX_PATHNAME)"
-        >
-          开始使用
-        </Button>
-      </div>
+      <Title class="title">问卷调查 | 在线投票</Title>
+      <Paragraph class="auto-typing">
+        已累计创建问卷100份，发布问卷90份，收到答卷900份
+      </Paragraph>
+    </div>
+    <div>
+      <Button
+        type="primary"
+        size="large"
+        @click="() => router.push(MANAGER_INDEX_PATHNAME)"
+      >
+        开始使用
+      </Button>
     </div>
   </div>
 </template>
