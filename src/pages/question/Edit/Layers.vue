1e286648b4775838274e8148624465b19884a031
vivion <vivonhyt@gmail.com>
2025-04-09 19:58:32 +0800

    实现图层组件的激活状态和编辑功能，添加点击事件处理

--- src/pages/question/Edit/Layers.vue
+++ src/pages/question/Edit/Layers.vue
@@ -1,21 +1,32 @@
 <template>
   <div class="layers">
-    <div class="layers-item" v-for="item in componentList" :key="item.fe_id">
+    <div
+      class="layers-item"
+      v-for="item in componentList"
+      :key="item.fe_id"
+      :class="{ isActive: activeId === item.fe_id }"
+      @click.stop="handleSelect(item.fe_id)"
+    >
       <span class="left">
-        {{ item.title }}
+        <template v-if="editingId !== item.fe_id">
+          {{ item.title }}
+        </template>
+        <template v-else>
+          <el-input v-model="item.title" />
+        </template>
       </span>
       <span class="right">
         <el-icon
           class="icon"
           :class="{ isClick: item.isHidden }"
-          @click="handleHide(item.fe_id, item.isHidden)"
+          @click.stop="handleHide(item.fe_id, item.isHidden)"
         >
           <Hide />
         </el-icon>
         <el-icon
           class="icon"
           :class="{ isClick: item.isLocked }"
-          @click="handleLock(item.fe_id, item.isLocked)"
+          @click.stop="handleLock(item.fe_id, item.isLocked)"
         >
           <Lock />
         </el-icon>
@@ -27,11 +38,27 @@
 <script setup lang="ts">
 import { useStore } from "vuex";
 import { useGetComponentInfo } from "@/hooks/useGetComponentInfo";
-import { ref } from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
 const store = useStore();
 const { componentList } = useGetComponentInfo();
 
-const isEdit = ref(false);
+const editingId = ref("");
+const activeId = ref("");
+// 处理document点击事件
+const handleDocumentClick = () => {
+  editingId.value = "";
+  activeId.value = "";
+};
+
+// 组件挂载时添加事件监听
+onMounted(() => {
+  document.addEventListener("click", handleDocumentClick);
+});
+
+// 组件卸载时移除事件监听
+onUnmounted(() => {
+  document.removeEventListener("click", handleDocumentClick);
+});
 
 const handleHide = (fe_id: string, isHidden: boolean) => {
   store.dispatch("componentsStore/hideComponent", {
@@ -45,6 +72,18 @@ const handleLock = (fe_id: string, isLocked: boolean) => {
     fe_id,
     isLocked,
   });
+};
+
+const handleSelect = (fe_id: string) => {
+  if (activeId.value === fe_id) {
+    // 如果已经激活，则进入编辑模式
+    editingId.value = fe_id;
+  } else {
+    // 第一次点击，只激活不编辑
+    activeId.value = fe_id;
+    editingId.value = "";
+  }
+  store.dispatch("componentsStore/changeSelectedID", { selectedId: fe_id });
 };
 </script>
 
--- src/pages/question/Edit/RightPanel.vue
+++ src/pages/question/Edit/RightPanel.vue
@@ -1,6 +1,6 @@
 <template>
-  <Tabs>
-    <Tabs.TabPane key="property">
+  <Tabs v-model:activeKey="activeKey">
+    <Tabs.TabPane :key="TAB_KEY.property">
       <template #tab>
         <span>
           <FileTextOutlined />
@@ -9,7 +9,7 @@
       </template>
       <ComponentProps />
     </Tabs.TabPane>
-    <Tabs.TabPane key="page">
+    <Tabs.TabPane :key="TAB_KEY.page">
       <template #tab>
         <span>
           <SettingOutlined />
@@ -26,6 +26,24 @@ import { Tabs } from "ant-design-vue";
 import { FileTextOutlined, SettingOutlined } from "@ant-design/icons-vue";
 import ComponentProps from "./ComponentProps.vue";
 import PageSetting from "./PageSetting.vue";
+import { useGetComponentInfo } from "@/hooks/useGetComponentInfo";
+import { ref, watch } from "vue";
+const { selectedId } = useGetComponentInfo();
+
+enum TAB_KEY {
+  property = "property",
+  page = "page",
+}
+// 根据selectedId判断切换哪个tab
+const activeKey = ref(TAB_KEY.page);
+
+watch(selectedId, (newVal) => {
+  if (newVal) {
+    activeKey.value = TAB_KEY.property;
+  } else {
+    activeKey.value = TAB_KEY.page;
+  }
+});
 </script>
 
 <style scoped lang="scss"></style>
